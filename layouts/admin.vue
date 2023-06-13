e216a6fd0a2638141eff175418ff3ec2104e24f4
Nguyen Van Hoang <hoangitk17@gmail.com>
2023-06-13 07:56:01 +0700

    init project

---
+++ layouts/admin.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>
+
+    Admin layout
+
+    </h1>
+    <slot />
+  </div>
+</template>
---
+++ pages/admin/dashboard/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-4">
+    Custom layout defined with <code>definePageMeta</code>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'admin'
+})
+</script>
